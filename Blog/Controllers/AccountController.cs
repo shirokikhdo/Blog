68707dbdf3997df49130bbd65ae3c5b86873057d
shirokikhdo <shirokikhdo@gmail.com>
2025-01-04 18:47:45 +0300

    Создаем AccountController

---
+++ Blog/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public ActionResult<UserModel> Create(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPatch]
+        public IActionResult Update(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult GetToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Blog/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.Models;
+
+public class UserModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public string Description { get; set; }
+
+    public byte[]? Photo { get; set; }
+}
