c8c680b70e8d3559fc7efaab1c2290f48cbec707
shirokikhdo <shirokikhdo@gmail.com>
2025-01-05 12:30:42 +0300

    NewsController. Передача параметров и аргументов в запросе

---
+++ Blog/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class NewsController : ControllerBase
+    {
+        [HttpGet("{userId}")]
+        public IActionResult GetByAuthor(int userId)
+        {
+            return Ok(userId);
+        }
+    }
+}
