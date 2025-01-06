2a857e85e50cfdb73fb00bea54af2708ad6298bb
shirokikhdo <shirokikhdo@gmail.com>
2025-01-06 17:20:33 +0300

    Подготовка проекта для фронтенда

---
+++ Api/Extensions/ApplicationServiceCollectionExtension.cs
@@ -0,0 +1,48 @@
+﻿using Api.Data;
+using Api.Models;
+using Api.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Api.Extensions;
+
+public static class ApplicationServiceCollectionExtension
+{
+    public static IServiceCollection AddServiceCollection(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = AuthOptions.ISSUER,
+                    ValidateAudience = true,
+                    ValidAudience = AuthOptions.AUDIENCE,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                    ValidateIssuerSigningKey = true
+                };
+            });
+        services.AddDbContext<BlogDbContext>(
+            options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("SqlServerConnection")));
+        services.AddTransient<UserService>();
+        services.AddTransient<NewsService>();
+        services.AddTransient<NoSqlDataService>();
+        services.AddCors(options =>
+            options.AddPolicy("CorsPolicy", policy =>
+                policy.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithOrigins(configuration["ClientHost"])));
+
+        return services;
+    }
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,36 +1,7 @@
-using Api.Data;
-using Api.Models;
-using Api.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.RequireHttpsMetadata = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = AuthOptions.ISSUER,
-            ValidateAudience = true,
-            ValidAudience = AuthOptions.AUDIENCE,
-            ValidateLifetime = true,
-            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-            ValidateIssuerSigningKey = true
-        };
-    });
-builder.Services.AddDbContext<BlogDbContext>(
-    options => 
-        options.UseSqlServer(
-            builder.Configuration.GetConnectionString("SqlServerConnection")));
-builder.Services.AddTransient<UserService>();
-builder.Services.AddTransient<NewsService>();
-builder.Services.AddTransient<NoSqlDataService>();
+builder.Services.AddServiceCollection(builder.Configuration);
 
 var app = builder.Build();
 app.UseSwagger();
@@ -39,4 +10,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.UseHttpsRedirection();
 app.MapControllers();
+app.UseCors("CorsPolicy");
 app.Run();
